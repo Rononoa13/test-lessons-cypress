eb8435bf9232f7f68a401e72b55326b769dd33c8
Rononoa13 <sumitluitel42@gmail.com>
2022-01-12 19:24:01 +0545

    Udemy 2 react lesson first state

---
+++ hatemReact/cypress-tutorials/cypress/integration/basics.js
@@ -0,0 +1,14 @@
+describe("Login Feature Test", () => {
+    
+    it("should visit the login page correctly", function(){
+        cy.log("testcase1: should visit the website correctly ")
+    })
+    
+    it("should log I love cypress", () => {
+        cy.log("testcase1: I love cypress ")
+    })
+})
+
+
+
+
