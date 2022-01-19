0f721251ccbac053a1b435fc54f78773370b3151
Rononoa13 <sumitluitel42@gmail.com>
2022-01-19 13:39:50 +0545

    selectors complete

--- hatemReact/cypress-tutorials/cypress/integration/basics.test.js
+++ hatemReact/cypress-tutorials/cypress/integration/basics.test.js
@@ -2,17 +2,34 @@
 
 describe("Login Feature Test", () => {
 
+    /**
+     * next
+     * nextAll
+     * nextUntil
+     * 
+     * prev
+     * prevAll
+     * prevUntil
+     */
+
         it("should visit the login page correctly on ", () => {
             cy.visit("cypress/index.html")
-            cy.get('.header-1.main')
-            cy.get('.list1.web')
-            cy.get('.course-list > .list1')
-            cy.get('.course-list').children('.list1')
-            cy.get('li').first()
-            cy.get('li').last()
-            cy.get('li').eq(21)
-            cy.get('li').filter('.mobile')
-            cy.get('li').filter('.web')
+            // cy.get('.header-1.main')
+            // cy.get('.list1.web')
+            // cy.get('.course-list > .list1')
+            // cy.get('.course-list').children('.list1')
+            // cy.get('li').first()
+            // cy.get('li').last()
+            // cy.get('li').eq(21)
+            // cy.get('li').filter('.mobile')
+            // cy.get('li').filter('.web')
+
+            // cy.get('.list1.web').nextAll()
+
+            // cy.contains('Selenium')
+            // cy.get('[attribute="value"]')
+            cy.get('[data-cy="selenium-course"]')
+            cy.get('.list3', {timeout:30000})
 
 
         })
